50554eaaa7233b3d44e5cfbff9487f1c469a43a2
alezanper <fabenavideso@gmail.com>
2019-01-13 17:42:01 -0500

    update skill 1.1

--- skill 1.1_Create Transact-SQL SELECT queries.sql
+++ skill 1.1_Create Transact-SQL SELECT queries.sql
@@ -2,54 +2,57 @@ USE TEST;
 
 --First name starting with A or B
 SELECT *
-FROM DB.people P
+FROM DB.EMPLOYEES P
 WHERE p.name LIKE '[AB]%'
 
 --Filter by date
 SELECT *
-FROM DB.PEOPLE P
+FROM DB.EMPLOYEES P
 WHERE P.birthdate BETWEEN '19830101' and '19861231'
 
 --Order (default is descending)
 SELECT *
-FROM DB.PEOPLE P
+FROM DB.EMPLOYEES P
 ORDER BY p.lastname
 
 --Using Top(3)
-SELECT TOP (3) p.id, p.name, p.lastname, p.birthdate
-FROM DB.PEOPLE P
+SELECT TOP (3) p.personid, p.name, p.lastname, p.birthdate
+FROM DB.EMPLOYEES P
 ORDER BY p.birthdate DESC;
 
 --Using with ties (For returning all values)
-SELECT TOP (3) WITH TIES p.id, p.name, p.lastname, p.birthdate
-FROM DB.PEOPLE P
+SELECT TOP (3) WITH TIES p.personid, p.name, p.lastname, p.birthdate
+FROM DB.EMPLOYEES P
 ORDER BY p.birthdate DESC;
 
 --Using Percent
-SELECT TOP (10) PERCENT p.id, p.name, p.lastname, p.birthdate
-FROM DB.PEOPLE P
+SELECT TOP (10) PERCENT p.personid, p.name, p.lastname, p.birthdate
+FROM DB.EMPLOYEES P
 ORDER BY p.birthdate DESC;
 
 --Using OFFSET-FETCH (Offset first 5 and return the next 2)
-SELECT p.id, p.name, p.lastname, p.birthdate
-FROM DB.PEOPLE P
-ORDER BY p.birthdate DESC, p.id DESC
+SELECT p.personid, p.name, p.lastname, p.birthdate
+FROM DB.EMPLOYEES P
+ORDER BY p.birthdate DESC, p.personid DESC
 OFFSET 5 ROWS FETCH NEXT 2 ROWS ONLY;
 
---UNION and UNION ALL
---71 ROWS (Implicit distinct)
-SELECT country, region, city
-FROM HR.Employees
-UNION 
-SELECT country, region, city
-FROM Sales.Customers;
+-------------------------
+---UNION and UNION ALL---
+-------------------------
 
---100 ROWS (Full columns)
-SELECT country, region, city
-FROM HR.Employees
+--10 ROWS (Implicit distinct)
+SELECT p.personid, p.name, p.lastname
+FROM DB.EMPLOYEES p
+UNION 
+SELECT p.personid, p.name, p.lastname
+FROM DB.EMPLOYEES p
+
+--20 ROWS (Full rows)
+SELECT p.personid, p.name, p.lastname
+FROM DB.EMPLOYEES p
 UNION ALL
-SELECT country, region, city
-FROM Sales.Customers;
+SELECT p.personid, p.name, p.lastname
+FROM DB.EMPLOYEES p
 
 --INTERSECT (Implicit distinct, it includes NULLS)
 SELECT country, region, city
