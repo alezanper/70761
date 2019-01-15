b96ff8b8040a293bff19dc51ba5faf83a3cb7e7a
alezanper <fabenavideso@gmail.com>
2019-01-14 22:25:07 -0500

    Skill 1.3 updated

--- skill 1.3.sql
+++ skill 1.3.sql
@@ -1,44 +1,57 @@
-USE TSQLV4;
-
+USE TEST;
+--------------------------
+---Conversion Functions---
+--------------------------
 SELECT TRY_CONVERT(DATE, '14/02/2017', 101) AS col1,	--NULL
 TRY_CONVERT(DATE, '02/14/2017', 101) AS col2;	--2017-02-14
 
+--------------------
+---Date Functions---
+--------------------
 SELECT FORMAT(SYSDATETIME(), 'yyyy-MM-dd');	--2019-01-06
-SELECT DATEPART(month, '20170212');	--2
-SELECT DATENAME(month, '20170212');	--February
-SELECT DATEFROMPARTS(2017, 02, 12);	--2017-02-12
+SELECT DATEPART(month, '20190212');	--2
+SELECT DATENAME(month, '20190212');	--February
+SELECT DATEFROMPARTS(2019, 02, 12);	--2017-02-12
 SELECT EOMONTH(SYSDATETIME());	--2019-01-31 (End of month)
-SELECT DATEADD(year, 1, '20170212');	--2018-02-12 00:00:00.000
-SELECT DATEDIFF(day, '20160212', '20170212');	--366
-SELECT DATEDIFF(year, '20161231', '20170101');	--1
+SELECT DATEADD(year, 1, '20190212');	--2020-02-12 00:00:00.000
+SELECT DATEDIFF(day, '20190212', '20190212');	--366
+SELECT DATEDIFF(year, '20191231', '20190101');	--1
 SELECT SWITCHOFFSET(SYSDATETIMEOFFSET(), '-08:00');
 
---CHAR FUNCTIONS
+--------------------
+---CHAR FUNCTIONS---
+--------------------
 SELECT SUBSTRING('abcde', 1, 3);	--abc
 SELECT LEFT('abcde', 3);	--abc
 SELECT RIGHT('abcde', 3);	--cde
 SELECT CHARINDEX(' ', 'Alexander Benavides');	--10
-SELECT LEN('Alex')	--4
+SELECT LEN('Alex');	--4
 SELECT REPLACE('A.L.E.X', '.', ' ');	--A L E X
 SELECT REPLICATE('A', 5);	--AAAAA
 SELECT FORMAT(2019, 'd10');	--0000002019
 
---CASE EXPRESSIONS
-SELECT productid, productname, unitprice, discontinued,
-CASE discontinued
-	WHEN 0 THEN 'No'
-	WHEN 1 THEN 'Yes'
+----------------------
+---CASE EXPRESSIONS---
+----------------------
+SELECT C.*,
+CASE c.contacttype
+	WHEN 'A' THEN 'Address'
+	WHEN 'C' THEN 'Phone'
 	ELSE 'Unknown'
-END AS discontinued_desc
-FROM Production.Products;
+END AS Contact_Description
+FROM DB.CONTACT C;
 
---Coalesce and Isnull
+-------------------------
+---Coalesce and Isnull---
+-------------------------
 DECLARE
 @x AS VARCHAR(3) = NULL,
 @y AS VARCHAR(10) = '1234567890';
 SELECT COALESCE(@x, @y) AS [COALESCE], ISNULL(@x, @y) AS [ISNULL];
 
---SYSTEM FUNCTIONS
+---------------------
+--SYSTEM FUNCTIONS---
+---------------------
 --@@ROWCOUNT
 --returns the number of rows affected by the last statement that you executed.
 SELECT empid, firstname, lastname
@@ -58,9 +71,3 @@ SELECT CAST(9 AS NUMERIC(12, 2)) / CAST(2 AS NUMERIC(12, 2));	--4.50000000000000
 
 SELECT RAND(1759);
 SELECT RAND();
-
-
-
-
-
-
