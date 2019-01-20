6e43a2444f6cb321df3df247098aaf95c9891e1a
alezanper <fabenavideso@gmail.com>
2019-01-20 10:49:11 -0500

    Skill 2.3 updated

--- skill 2.3 Group and pivot data by using queries.sql
+++ skill 2.3 Group and pivot data by using queries.sql
@@ -35,11 +35,13 @@ FROM  DB.EMPLOYEES E
 GROUP BY CUBE( e.jobtitle, E.location )
 ORDER BY 1, 3;
 
---Using ROLLUP
+------------------
+---Using ROLLUP---
+------------------
 --Rollup makes all possible combinations when there’s a natural hierarchy
-SELECT shipcountry, shipregion, shipcity, COUNT(*) AS numorders
-FROM Sales.Orders
-GROUP BY ROLLUP( shipcountry, shipregion, shipcity );
+SELECT E.location, E.jobtitle, COUNT(*) 
+FROM db.EMPLOYEES E
+GROUP BY ROLLUP( E.location, E.jobtitle );
 
 --Pivoting Data
 WITH PivotData AS
@@ -56,47 +58,43 @@ FROM PivotData
 		FOR < spreading column > IN (< distinct spreading values >) ) AS P;
 
 --Example
-WITH PivotData AS
-(
-	SELECT
-		custid, -- grouping column
-		shipperid, -- spreading column
-		freight -- aggregation column
-	FROM Sales.Orders
-)
-SELECT custid, [1], [2], [3]
-FROM PivotData
-	PIVOT(SUM(freight) FOR shipperid IN ([1],[2],[3]) ) AS P;
+---------------------------
 
 --UnPivoting
 SELECT < column list >, < names column >, < values column >
 FROM < source table >
 UNPIVOT( < values column > FOR < names column > IN( <source columns> ) ) AS U;
 
-SELECT custid, shipperid, freight
-FROM Sales.FreightTotals
-UNPIVOT( freight FOR shipperid IN([1],[2],[3]) ) AS U;
+--Example
+---------------------------
 
---Using Window Functions
-
+----------------------------
+---Using Window Functions---
+----------------------------
 --Window aggregate functions
-SELECT custid, orderid, val,
-	SUM(val) OVER(PARTITION BY custid) AS custtotal,
-	SUM(val) OVER() AS grandtotal
-FROM Sales.OrderValues;
+SELECT	E.boid, 
+		E.jobtitle, 
+		E.salary,
+		SUM(E.SALARY) OVER(PARTITION BY E.jobtitle) AS areatotal,
+		SUM(E.SALARY) OVER() AS totalcompany
+FROM DB.EMPLOYEES E;
 
---Window Ranking Functions
-SELECT custid, orderid, val,
-	ROW_NUMBER() OVER(ORDER BY val) AS rownum,
-	RANK() OVER(ORDER BY val) AS rnk,
-	DENSE_RANK() OVER(ORDER BY val) AS densernk,
-	NTILE(100) OVER(ORDER BY val) AS ntile100
-FROM Sales.OrderValues;
+------------------------------------
+---Using Window Ranking Functions---
+------------------------------------
+SELECT E.boid, E.jobtitle,
+	ROW_NUMBER() OVER(ORDER BY E.jobtitle) AS rownum,
+	RANK() OVER(ORDER BY E.jobtitle) AS rnk,
+	DENSE_RANK() OVER(ORDER BY E.jobtitle) AS densernk,
+	NTILE(100) OVER(ORDER BY E.jobtitle) AS ntile100
+FROM DB.EMPLOYEES E
 
---Window Offset Functions
-SELECT custid, orderid, orderdate, val,
-LAG(val) OVER(PARTITION BY custid
-ORDER BY orderdate, orderid) AS prev_val,
-LEAD(val) OVER(PARTITION BY custid
-ORDER BY orderdate, orderid) AS next_val
-FROM Sales.OrderValues;
+-----------------------------
+---Window Offset Functions---
+-----------------------------
+SELECT E.boid, E.jobtitle, E.salary,
+LAG(E.salary) OVER(PARTITION BY E.jobtitle
+ORDER BY E.salary) AS prev_val,
+LEAD(E.salary) OVER(PARTITION BY E.jobtitle
+ORDER BY E.salary) AS next_val
+FROM DB.EMPLOYEES E
