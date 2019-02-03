cc898085dd13620c4f9250f53ebdf5c6cc32df04
alezanper <fabenavideso@gmail.com>
2019-02-03 17:54:30 -0500

    skill 3.3 update

--- skill 3.3 Implement data types and NULLs.sql
+++ skill 3.3 Implement data types and NULLs.sql
@@ -8,3 +8,11 @@ SELECT CAST(@f AS NUMERIC(28, 14)) AS numericvalue;
 SELECT CAST('Alex' AS INT);			--Generates an error
 SELECT TRY_CAST('Alex' AS INT);		--Returns NULL instead of an error
 
+--Null handling
+SELECT ISNULL(10, NULL);			--10
+SELECT ISNULL(NULL, 10);			--10
+SELECT ISNULL(NULL, NULL);			--NULL
+
+SELECT COALESCE(NULL, 10, 12);		--10
+SELECT COALESCE(NULL, NULL, 12);	--12
+SELECT COALESCE(10, 12, NULL);		--10
